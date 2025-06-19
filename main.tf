03037c98b81d1d1c4805a36143f75430df58ad28
Charan Kumar H <charankumarh4@gmail.com>
2025-06-19 18:05:51 +0530

    added root table and igw name

--- main.tf
+++ main.tf
@@ -8,8 +8,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "main" {
   vpc_id   = aws_vpc.main.id
-  for_each = var.subnet_config #key={cidr, az} each.key each.value 
-
+  for_each = var.subnet_config 
   cidr_block        = each.value.cidr_block
   availability_zone = each.value.az
 
@@ -20,11 +19,11 @@ resource "aws_subnet" "main" {
 
 locals {
   public_subnet = {
-    #key={} if public is true in subnet_config
+   
     for key, config in var.subnet_config : key => config if config.public
   }
   private_subnet = {
-    #key={} if public is true in subnet_config
+    
     for key, config in var.subnet_config : key => config if !config.public
   }
 }
@@ -33,6 +32,10 @@ locals {
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
   count  = length(local.public_subnet) > 0 ? 1 : 0
+    tags = {
+    Name = "Internete Gateway"
+  }
+  
 }
 
 #Routing table
@@ -43,10 +46,13 @@ resource "aws_route_table" "main" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main[0].id
   }
+    tags = {
+    Name = "Public Route Table"
+  }
 }
 
 resource "aws_route_table_association" "main" {
-  for_each = local.public_subnet #public_subnet={} private_subnet={}
+  for_each = local.public_subnet 
 
   subnet_id      = aws_subnet.main[each.key].id
   route_table_id = aws_route_table.main[0].id
